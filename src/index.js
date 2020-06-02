c788491c30b7246d2a0a58bd13d9f7f853375f25
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2020-06-02 15:59:32 -0500

    reto resuelto

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const findNumber = function (number) {
-    //aqui escribiras tu codigo
-}; 
+    var isPrim = false;
+    if (number == 2) {
+        return true;
+    }
+    for (let index = 2; index < number; index++) {
+        if (isPrim % index == 0) {
+            return true
+        }
+    }
+    return isPrim;
+};
 
 module.exports = findNumber;
