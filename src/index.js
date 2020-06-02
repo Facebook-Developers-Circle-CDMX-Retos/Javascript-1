b0810b396079f1ad3c334308cc0a78fb0b3e3ca0
Nestor <programatonpro@gmail.com>
2020-06-02 17:00:45 -0500

    finalizado..

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const findNumber = function (number) {
     //aqui escribiras tu codigo
+    if(number<= 1) return false;
+
+    if(number<= 3) return true;
+
+    if(number%2 ===0 || number%3 ===0) return false;
+
+    for(let i=5;i*i<=number;i=i+6){
+
+    	if(number%i === 0  || number%(i+2) === 0)
+    		return false;
+    } 
+    
+    return true;
 }; 
 
 module.exports = findNumber;
