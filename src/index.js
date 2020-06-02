e43553c85fcdc7bc6b3b0ecb55f6a225af9e1096
Daniel Ochoa <danielochoa01@yahoo.com>
2020-06-02 14:17:22 -0600

    Implementada la solución para verificar si un número es primo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 const findNumber = function (number) {
     //aqui escribiras tu codigo
+    if (number <= 1) {
+        return false
+    }
+        
+
+    if (number <= 3){
+        return true
+    } 
+        
+
+    if (number % 2 == 0 || number % 3 == 0){
+        return false
+    }
+        
+
+    let i = 5
+    while (i * i <= number) {
+        if(number % i == 0 || number % (i + 2) == 0){
+            return false
+        }
+            
+        i += 6
+    }
+
+    return true
+
 }; 
 
 module.exports = findNumber;
