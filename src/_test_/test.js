b67d7e2e60a56fad78201ba54f3b5d0fda79d177
Josh <cesar.joshua.p.c@gmail.com>
2020-06-02 17:00:57 -0500

    Update test.js
    We update the test for a better filter

--- src/_test_/test.js
+++ src/_test_/test.js
@@ -6,10 +6,13 @@ describe('App', function () {
         expect(testFunction(1)).toBe(false);
         expect(testFunction(2)).toBe(true);
         expect(testFunction(3)).toBe(true);
+        expect(testFunction(4)).toBe(false);
         expect(testFunction(5)).toBe(true);
         expect(testFunction(11)).toBe(true);
+        expect(testFunction(15)).toBe(false);
         expect(testFunction(191)).toBe(true);
         expect(testFunction(191)).toBe(true);
+        expect(testFunction(200)).toBe(false);
         expect(testFunction(199)).toBe(true);
     })
 })
