317cf598b7a7f170d8b448db429030a3187bd31e
Marco Tzuc <mirzoz12@gmail.com>
2020-06-02 16:47:17 -0500

    Check if number is prime

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const findNumber = function (number) {
-    //aqui escribiras tu codigo
-}; 
+  let x = 0;
+  for(let i = 1; i<=number;i++){
+    if(number%i === 0) {
+      x++;
+    }
+  }
+  return x <= 2;
+};
 
 module.exports = findNumber;
