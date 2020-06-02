340e6fc15c83944d499de710bf1ceb3e8c6ac234
devfer <ferkki@hotmail.com>
2020-06-02 17:09:33 -0400

    codigo formateado

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const findNumber = function (number) {
   let counter = 0;
   for (let i = 1; i <= number; i++) {
-    if (number % i === 0) counter+=1;
-}
+    if (number % i === 0) counter += 1;
+  }
   return counter === 2 ? true : false;
 };
 
