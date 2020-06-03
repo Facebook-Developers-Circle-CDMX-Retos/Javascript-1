b6d52d83d04aa8a6ffd8079fb3ef158f5b0c36c2
Erick Alejandro Cuadrado Vilchis <erick.cuadrado@globant.com>
2020-06-03 00:36:56 -0500

    Solucion prime number

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const findNumber = function (number) {
-    //aqui escribiras tu codigo
+    for(let i =2; i<number; i++){
+	   if(number % i == 0) 
+	    return false
+	  return number==1 ? false: true
+    }
 }; 
 
 module.exports = findNumber;
