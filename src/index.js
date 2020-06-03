1d032a704cc06758630f8c5b526f50f028e51df8
enrique26 <enriqueibilis@gmail.com>
2020-06-02 20:40:04 -0500

    Update index.js
    es numero primo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const findNumber = function (number) {
     //aqui escribiras tu codigo
+    var esprimo = false
+    if(number == 2){
+        esprimo = true
+    }else if( number == 1){
+        esprimo = false
+    }else if(number > 1 && number != 2){
+        for(var i=2;i<number;i++){
+            if(number % i == 0){
+                esprimo = false
+                break;
+            }else{
+                esprimo = true
+            }
+        }
+        
+    }
+
+    return esprimo
 }; 
 
 module.exports = findNumber;
