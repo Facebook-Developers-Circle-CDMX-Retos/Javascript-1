4cff81320086bf60e70a0b8bb0389855680ee0a2
Erick Alejandro Cuadrado Vilchis <erick.cuadrado@globant.com>
2020-06-03 01:08:29 -0500

    Correction of test 1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const findNumber = function (number) {
-    for(let i =2; i<number; i++){
-	   if(number % i == 0) 
+    for(let i =2; i < number; i++)
+	   if(number % i === 0) 
 	    return false
-	  return number==1 ? false: true
-    }
+	  return number === 1 ? false:true
 }; 
 
 module.exports = findNumber;
