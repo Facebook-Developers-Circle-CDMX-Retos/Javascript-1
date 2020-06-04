43ab42d4b3aeb57aad901f94ce6b1176f6cd1b0b
mauriciogalo <ogalo96@gmail.com>
2020-06-03 20:58:17 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const findNumber = function (number) {
-    //aqui escribiras tu codigo
+    if(number == 1){
+        return false;
+    } else if (number == 2 || number == 3){
+        return true;
+    } else {
+        return  Number.isInteger(Math.abs((number+1)/6)) || Number.isInteger(Math.abs((number-1)/6));
+    }
 }; 
 
 module.exports = findNumber;
